8f885d9117491664bbec131359875997f0089029
Mạnh Quỳnh SCs <abc2662@hotmail.com.vn>
2018-06-26 00:20:55 +0700

    added ntree

---
+++ Gomoku/AI/GomokuAIv1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gomoku.Board;
+
+namespace Gomoku.AI
+{
+    public class GomokuAIv1
+    {
+        public GomokuAIv1()
+        {
+        }
+
+        public Tile Play(Board.Board board, Player player)
+        {
+            return null;
+        }
+        
+    }
+}
---
+++ Gomoku/AI/NTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku.AI
+{
+    public class NTree<T>
+    {
+        public T Value;
+        public LinkedList<NTree<T>> Nodes;
+
+        public NTree(T value)
+        {
+            Value = value;
+            Nodes = new LinkedList<NTree<T>>();
+        }
+    }
+}
